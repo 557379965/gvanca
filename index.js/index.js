02b2051ccf04a29a5041730d4210fb014b663dc2
gantsa <tskhadadze.gvanca@bk.ru>
2023-11-08 21:41:59 +0400

    feat: davaleba

---
+++ index.js/index.js
@@ -0,0 +1,17 @@
+const a =1;
+const b =4;
+const c =3;
+
+const gvanca = (b**b) - (4*a*c);
+
+
+if(gvanca> 0){
+  console.log ('d მეტია 0')
+
+}else if(gvanca = 0){
+    console.log('d უდრისo 0-ზე')
+}else if(d < 0){
+    console.log('d ნაკლებია 0-ზე')
+}
+
+
